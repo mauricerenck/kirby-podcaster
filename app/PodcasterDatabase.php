93a91b151f666b3c3ba6085a61a5541f5080ccca
Maurice Renck <maurice@maurice-renck.de>
2023-03-28 21:47:36 +0200

    chore: code cleanup

--- app/PodcasterDatabase.php
+++ app/PodcasterDatabase.php
@@ -11,12 +11,12 @@ class PodcasterDatabase
         try {
             if ($dbType === 'mysql') {
                 $database = new Database([
-                                             'type' => 'mysql',
-                                             'host' => option('mauricerenck.podcaster.statsHost'),
-                                             'database' => option('mauricerenck.podcaster.statsDatabase'),
-                                             'user' => option('mauricerenck.podcaster.statsUser'),
-                                             'password' => option('mauricerenck.podcaster.statsPassword'),
-                                         ]);
+                    'type' => 'mysql',
+                    'host' => option('mauricerenck.podcaster.statsHost'),
+                    'database' => option('mauricerenck.podcaster.statsDatabase'),
+                    'user' => option('mauricerenck.podcaster.statsUser'),
+                    'password' => option('mauricerenck.podcaster.statsPassword'),
+                ]);
 
                 $database->execute('SET sql_mode=(SELECT REPLACE(@@sql_mode, "ONLY_FULL_GROUP_BY", ""))');
 
@@ -26,10 +26,11 @@ class PodcasterDatabase
             $sqlitePath = option('mauricerenck.podcaster.sqlitePath');
 
             return new Database(['type' => 'sqlite', 'database' => $sqlitePath . 'podcaster.sqlite',]);
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             echo 'Could not connect to Database: ', $e->getMessage(), "\n";
 
             return null;
         }
     }
+
 }
